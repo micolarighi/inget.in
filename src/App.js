0cb91c620ed384549b607a541fd331755d9c71ac
paradoxicalinsomniac <micolaarighi@gmail.com>
2022-05-29 19:06:52 +0700

    bug fixes when adding new task

--- src/App.js
+++ src/App.js
@@ -4,22 +4,21 @@ import AppHeader from './components/AppHeader'
 import Footer from './components/Footer'
 import AppContent from './components/AppContent';
 import { Toaster } from 'react-hot-toast'
-import { motion } from 'framer-motion'
 
 
 function App() {
   return (
-    <>
-      <motion.div animate={{ scale: [0.8, 1] }} className="App mt-4 bg-white duration-300 lg:px-10 lg:py-10 px-5 py-5 rounded-lg shadow-2xl ">
+    <div>
+      <div className="App mt-4 bg-white duration-300 lg:px-10 lg:py-10 px-5 py-5 rounded-lg shadow-2xl dark:bg-white ">
         <div className='flex lg:flex-row md:flex-row flex-col justify-center items-center mb-5'>
           <Title />
           <AppHeader />
         </div>
         <Footer />
         <AppContent />
-      </motion.div>
+      </div>
       <Toaster toastOptions={{ style: { fontSize: '1.4rem' } }} />
-    </>
+    </div>
   );
 }
 
