0db496c3b99784ad84b1ea0cb3678aab598e555c
paradoxicalinsomniac <81902126+micolarighi@users.noreply.github.com>
2022-05-31 14:14:49 +0700

    Update Title.js

--- src/components/Title.js
+++ src/components/Title.js
@@ -5,7 +5,7 @@ function Header() {
   return (
     <motion.div animate={{ scale: [0.8, 1] }} transition={{ duration: 0.5 }} className='flex justify-center items-center'>
       <div className='text-center'>
-        <h1 className='lg:text-7xl text-5xl  font-bold mt-4 text-slate-800'>MiDo List</h1>
+        <h1 className='lg:text-7xl text-5xl  font-bold mt-4 text-slate-800'>Inget.in</h1>
         <p className='lg:text-lg text-sm'>List What You Do and be <span className='font-bold'> Productive</span></p>
       </div>
     </motion.div>
